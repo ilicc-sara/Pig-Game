4dd9104e8b58783bd2dff7d4a4014765b999aaf8
Sara <sarailic431@gmail.com>
2024-11-10 18:58:17 +0100

    finishing css and starting JS

--- main.js
+++ main.js
@@ -1,2 +1,14 @@
 "use strict";
 import "./style.css";
+
+let player1;
+let player2;
+let currentScore1;
+let currentScore2;
+let score1;
+let score2;
+
+const dice = Math.trunc(Math.random() * 6) + 1;
+console.log(dice);
+
+const rollDiceBtn = document.querySelector(".roll-btn");
