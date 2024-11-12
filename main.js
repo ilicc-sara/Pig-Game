e55ac3f1ddf9e1ea83a9b02dbb3c64a2372610ac
Sara <sarailic431@gmail.com>
2024-11-12 15:50:45 +0100

    finished game

--- main.js
+++ main.js
@@ -1,19 +1,11 @@
 "use strict";
 import "./style.css";
-
-let player1 = 0;
-let player2 = 1;
-
-let score1 = 0;
-let score2 = 0;
-
-let currentScore1 = 0;
-let currentScore2 = 0;
-
 const diceEl = document.querySelector(".dice");
 const rollBtnEl = document.querySelector(".roll-btn");
 
 const holdBtnEl = document.querySelector(".hold-btn");
+
+const newGameEl = document.querySelector(".new-game-btn");
 
 const score1El = document.querySelector(".score-1");
 const score2El = document.querySelector(".score-2");
@@ -23,6 +15,15 @@ const currentScore2El = document.querySelector(".current-score-2");
 
 const player1El = document.querySelector(".player-1");
 const player2El = document.querySelector(".player-2");
+
+let player1 = 0;
+let player2 = 1;
+
+let score1 = 0;
+let score2 = 0;
+
+let currentScore1 = 0;
+let currentScore2 = 0;
 
 let activePlayer = player1;
 
@@ -38,7 +39,7 @@ const switchPlayer = function () {
   }
 };
 
-rollBtnEl.addEventListener("click", function () {
+const rollFunction = function () {
   const dice = Math.trunc(Math.random() * 6) + 1;
   console.log(dice);
   diceEl.textContent = dice;
@@ -65,9 +66,11 @@ rollBtnEl.addEventListener("click", function () {
     }
     switchPlayer();
   }
-});
+};
 
-holdBtnEl.addEventListener("click", function () {
+rollBtnEl.addEventListener("click", rollFunction);
+
+const holdFunction = function () {
   if (activePlayer === player1) {
     score1 = score1 + currentScore1;
     currentScore1 = 0;
@@ -85,6 +88,44 @@ holdBtnEl.addEventListener("click", function () {
   }
 
   if (score1 > 50 || score2 > 50) {
-    console.log("player winner");
+    rollBtnEl.removeEventListener("click", rollFunction);
+    holdBtnEl.removeEventListener("click", holdFunction);
+    diceEl.textContent = "";
+
+    if (score1 > 50 && score2 < 50) {
+      if (!player1El.classList.contains("active")) {
+        player1El.classList.add("active");
+        player2El.classList.remove("active");
+      }
+    } else if (score2 > 50 && score1 < 50) {
+      if (!player2El.classList.contains("active")) {
+        player2El.classList.add("active");
+        player1El.classList.remove("active");
+      }
+    }
   }
+};
+
+holdBtnEl.addEventListener("click", holdFunction);
+
+newGameEl.addEventListener("click", function () {
+  score1 = 0;
+  score2 = 0;
+  score1El.textContent = score1;
+  score2El.textContent = score2;
+
+  currentScore1 = 0;
+  currentScore2 = 0;
+  currentScore1El.textContent = currentScore1;
+  currentScore2El.textContent = currentScore2;
+
+  diceEl.textContent = "";
+  activePlayer = player1;
+  if (!player1El.classList.contains("active")) {
+    player1El.classList.add("active");
+    player2El.classList.remove("active");
+  }
+
+  rollBtnEl.addEventListener("click", rollFunction);
+  holdBtnEl.addEventListener("click", holdFunction);
 });
